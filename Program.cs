7648a6166f597bff331a04041e1fbf2f1a9d552e
Christian van der Walt <Slowmochris5@gmail.com>
2025-07-22 19:11:06 +0200

    Defined the function "Coupons"

--- Program.cs
+++ Program.cs
@@ -19,7 +19,30 @@ namespace PRG161_Project
             string[] customerNames = new string[10];
             string[] books = new string[10];
 
-            Console.WriteLine();
+            string numCoupon = Coupons(3);
+
+
+            
+        }
+
+        static string Coupons(int numRentals)
+        {
+            if (numRentals >= 10 && numRentals <= 24)
+            {
+                return "You recieved 1 Reward Rental.";
+            }
+            else if (numRentals > 24 && numRentals <= 49)
+            {
+                return "You recieved 2 Reward Rental.";
+            }
+            else if (numRentals > 49 && numRentals <= 74)
+            {
+                return "You recieved 4 Reward Rental.";
+            }
+            else
+            {
+                return "You recieved 8 Reward Rental.";
+            }
         }
     }
 }
